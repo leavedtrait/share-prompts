511e275d98a5cf2ca8027826c643e241656a27cf
leavedtrait <leavedtrait@gmail.com>
2024-08-19 22:07:30 +0300

    ...

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,7 @@
 const nextConfig = {
     experimental: {
       appDir: true,
+      missingSuspenseWithCSRBailout: false,
       serverComponentsExternalPackages: ["mongoose"],
     },
     images: {
