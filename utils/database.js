fd3f773606f972adecf055b60d984fd38d00a9b0
leavedtrait <leavedtrait@gmail.com>
2024-08-19 21:52:31 +0300

    finishing

--- utils/database.js
+++ utils/database.js
@@ -12,7 +12,9 @@ export const connectToDB = async () => {
 
   try {
     await mongoose.connect(process.env.MONGODB_URI, {
-      dbName: "Promptopia",
+      dbName: "share_prompt",
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
     })
 
     isConnected = true;
